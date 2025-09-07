e8dc68d878e4bf1ae04cc75251acf00837dd0bb6
prathamthakur215-gif <prathamthakur215@gmail.com>
2025-09-07 20:15:37 +0530

    Update 7th_Pattern.c

--- 7th_Pattern.c
+++ 7th_Pattern.c
@@ -1,15 +1,10 @@
-#include <stdio.h>
+/*
+Print this Pattern:
 
-int main() {
-    int i, j;
-    int n = 5;  // number of rows
-
-    for (i = n; i >= 1; i--) {      // outer loop for rows
-        for (j = 1; j <= i; j++) {  // inner loop for numbers
-            printf("%d ", j);
-        }
-        printf("\n");  // move to next line
-    }
-
-    return 0;
-}
+                *
+              * * *
+            * * * * *
+          * * * * * * *
+        * * * * * * * * *
+   
+*/
